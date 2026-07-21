922aa3e3560a531623cd38ab3cc5e8d7a1142e53
Gabriele <leonardi.gabriele9821@gmail.com>
2026-07-21 11:39:55 +0200

    display md none

--- src/components/FooterNav.jsx
+++ src/components/FooterNav.jsx
@@ -1,19 +1,19 @@
-import ChatIcon from "./ChatIcon"
-import HomeIcon from "./HomeIcon"
-import JobIcon from "./JobIcon"
-import NetworkIcon from "./NetworkIcon"
-import NotificationIcon from "./NotificationIcon"
+import ChatIcon from "./ChatIcon";
+import HomeIcon from "./HomeIcon";
+import JobIcon from "./JobIcon";
+import NetworkIcon from "./NetworkIcon";
+import NotificationIcon from "./NotificationIcon";
 
-function FooterNav () {
-    return (
-        <nav className="footerNav d-flex justify-content-around align-items-center d-lg-none">
-            <HomeIcon/>
-            <NetworkIcon/>
-            <ChatIcon/>
-            <NotificationIcon/>
-            <JobIcon/>
-        </nav>
-    )
+function FooterNav() {
+  return (
+    <nav className="footerNav d-flex justify-content-around align-items-center d-md-none d-lg-none">
+      <HomeIcon />
+      <NetworkIcon />
+      <ChatIcon />
+      <NotificationIcon />
+      <JobIcon />
+    </nav>
+  );
 }
 
-export default FooterNav
+export default FooterNav;
