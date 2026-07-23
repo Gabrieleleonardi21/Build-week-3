97f02c1d5dbf1675ffe28318a6f9e65cd9a676a4
Francis Nicole Paulino <nicoleppaulino@gmail.com>
2026-07-23 11:17:13 +0200

    aggiunta modale premium dopo login, chiudibile dopo 5 secondi

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,23 +2,27 @@ import { Row, Col } from "react-bootstrap";
 import RightAside from "@/features/feed/RightAside";
 import Feed from "@/features/feed/Feed";
 import Sidebar from "@/features/feed/Sidebar";
+import PremiumModal from "@/ui/PremiumModal";
 
 // Contenuto della rotta /home: le tre colonne del feed.
 // Header, Container e FooterNav stanno nel Layout condiviso.
 function Home() {
   return (
-    <Row>
-      <Col xs={12} md={4} lg={3}>
-        <Sidebar />
-      </Col>
-      <Col xs={12} md={8} lg={6}>
-        <Feed />
-      </Col>
-      <Col xs={12} md={4} className="InvisibleCol"></Col>
-      <Col xs={12} md={8} lg={3}>
-        <RightAside />
-      </Col>
-    </Row>
+    <>
+      <PremiumModal />
+      <Row>
+        <Col xs={12} md={4} lg={3}>
+          <Sidebar />
+        </Col>
+        <Col xs={12} md={8} lg={6}>
+          <Feed />
+        </Col>
+        <Col xs={12} md={4} className="InvisibleCol"></Col>
+        <Col xs={12} md={8} lg={3}>
+          <RightAside />
+        </Col>
+      </Row>
+    </>
   );
 }
 
